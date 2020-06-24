77fb761abc9106650ce532aa42c610f0e16e686d
githubvijay <stain18pc@hotmail.com>
2020-06-24 16:20:39 +0530

    First Comment

---
+++ src/build/precache-manifest.f3d72046fb067ee66ddf050267ba542a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3659233c78983b6a3dcb0d1533eb7ce7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "554641d81b785bd223ce",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "eeeab3d4cbd4b041489b",
+    "url": "/static/js/2.a257835f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a257835f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554641d81b785bd223ce",
+    "url": "/static/js/main.3d3f49f9.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
